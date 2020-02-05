d34e526bf7e29f120b636881a853537d7f1d6061
Feyyaz Yılmaz <45435495+Benchtengelen@users.noreply.github.com>
2020-02-05 11:29:02 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title></title>
+</head>
+<body>
+   <h1> Javascript </h1>
+   <script>
+
+   var bulls=["Jordan","Pippen","Rodman","Kukoc","Kerr"];
+      for ( var i=0; i<=4; i++)
+         document.write(bulls[i]+"<br>");
+
+</script>
+</body>
+</html>
